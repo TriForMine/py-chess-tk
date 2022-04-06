197081ebb9eeec11df25bf04e14b888ffca104d7
TriForMine <quentin@triformine.dev>
2022-04-06 10:34:36 +0200

    Optimize calculate total score

--- utils.py
+++ utils.py
@@ -10,8 +10,8 @@ def enemy_color(color: str):
 
 def calculate_total_score(grid: list[list[Piece | None]]):
     total_score = 0
-    for y in range(len(grid)):
-        for piece in grid[y]:
+    for line in grid:
+        for piece in line:
             if piece:
                 if piece.color == "black":
                     total_score -= piece.score
